67c24688961ab1820d16a8c6296da54b73159469
sharadhake <sharad.hake@gmail.com>
2016-10-02 15:19:18 +0200

    Login functionality

---
+++ js/functional/common.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Sharad on 10/2/2016.
+ */
+var serviceURL= "http://localhost:8090/Whatpay-BE/"
--- js/functional/login.js
+++ js/functional/login.js
@@ -1,3 +1,21 @@
 /**
  * Created by Sharad on 10/2/2016.
  */
+
+var loginURL = serviceURL+'userLogin.php';
+
+function loginUSER() {
+    var mobileNumber = $('#inputMobileNumber').val(),
+        accessCode = $('#inputAccessID').val(),
+        requestURL=loginURL + '?mobileNumber='+mobileNumber+'&accessCode='+accessCode;
+
+    $.getJSON(requestURL)
+
+        .done(function (data) {
+            alert(JSON.stringify(data));
+        })
+
+        .fail(function () {
+            alert('<p>Oh no, something went wrong!</p>');
+        });
+}
